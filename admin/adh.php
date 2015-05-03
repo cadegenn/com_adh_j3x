cdb25159c8a3d1a40a1ad04fd22417ca5a2b3a14
DEGENNES Charles-Antoine <cadegenn@gmail.com>
2015-05-03 10:55:32 +0200

    rewrite to joomla 3 compliant

--- admin/adh.php
+++ admin/adh.php
@@ -1,30 +1,22 @@
 <?php
-// No direct access to this file
-defined('_JEXEC') or die('Restricted access');
- 
-// Set some global property
-$document = JFactory::getDocument();
-//$document->addStyleDeclaration('.icon-48-categories, .icon-48-adh {background-image: url(components/com_adh/images/ico-48x48/adh.png);}');
-//$document->addStyleDeclaration('.ico-32-import {background-image: url(components/com_adh/images/ico-128x128/import-icon.png); width:32px;}');
-$document->addStyleSheet( 'components/com_adh/css/com_adh.css' );
+/** 
+ *  @component	com_adh
+ * 
+ *  @copyright	Copyright (C) 2012-2015 DEGENNES Charles-Antoine <cadegenn@gmail.com>
+ *  @license	Affero GNU General Public License Version 3; see LICENSE.txt
+ * 
+ */
 
-// require helper file
-JLoader::register('adhHelper', dirname(__FILE__).'/helpers/adh.php');
-// joomla lacks a group object with suitable methods like it have for users
-// we will not use it here however
-//JLoader::register('JGroup', dirname(__FILE__).'/helpers/group-joomla.php');
-//JLoader::register('JGroupHelper', dirname(__FILE__).'/helpers/group-joomla-helper.php');
+defined('_JEXEC') or die;
+JHtml::_('behavior.tabstate');
 
-// import joomla controller library
-jimport('joomla.application.component.controller');
- 
-// Get an instance of the controller prefixed by chantiers
-$controller = JController::getInstance('adh');
- 
-// Perform the Request task
-$controller->execute(JRequest::getCmd('task'));
- 
-// Redirect if set by the controller
+$input = JFactory::getApplication()->input;
+
+if (!JFactory::getUser()->authorise('core.manage', 'com_tags'))
+{
+	return JError::raiseWarning(404, JText::_('JERROR_ALERTNOAUTHOR'));
+}
+
+$controller	= JControllerLegacy::getInstance('Adh');
+$controller->execute($input->get('task'));
 $controller->redirect();
-
-?>
